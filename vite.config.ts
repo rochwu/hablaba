b836add53e0081483abc78bcb69abb3bf9081aea
Rolando Wu <rolando.che.wu@gmail.com>
2025-03-01 15:13:34 -0800

    Forgot base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/hablaba',
   plugins: [react()],
   server: {
     open: true, // Automatically opens browser
